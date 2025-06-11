a60f183e54054914637e178361bba694fd549fab
FatimaKas <noorfatima.kas.t23071@sophiacollege.edu.in>
2025-06-11 21:53:35 +0530

    Updated website with new changes

---
+++ script.js
@@ -0,0 +1,19 @@
+// form repeaterAdd commentMore actions
+$(document).ready(function(){
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show:function(){
+            $(this).slideDown();
+        },
+        hide: function(deleteElement){
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                //generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+})
